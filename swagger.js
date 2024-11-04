eaf5c20020223e4d67a9ee781d9d9e4be6266193
KarthikDamanellore <karthikdamanellore.viljetech@gmail.com>
2024-11-04 16:52:52 +0530

    swagger: host url changed

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description:
       "ebay-ecommerce- doubts send query damanellorekarthik@gmail.com",
   },
-  host: "ebay-ecommerce.onrender.com/",
+  host: "ebay-ecommerce.onrender.com",
   schemes: ["https"],
 };
 
